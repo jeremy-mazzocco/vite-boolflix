fe20d9cfd878e9b7a2feac845a74c3401661ff1b
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-06-11 19:28:45 +0100

    add store.noposter

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ export const store = reactive({
     movieURL: 'https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=',
     seriesURL: 'https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&query=',
     contentInput: '',
-    noPoster: '../../public/peace.png',
+    noPoster: 'no poster',
     dataMovies: [],
     dataSeries: [],
     flags: [
